83919e9ac6834f21b7885e5bc3da6f38a972906a
Jeet-Aaditya <aadijeet28@gmail.com>
2026-09-09 21:58:36 +0530

    Added Game logic

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let winningPatterns = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9],
-    [1,4,7],
-    [2,5,8],
-    [3,6,9],
-    [1,5,9],
-    [3,5,7]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
 ]
 
 let player = true;
@@ -15,16 +15,42 @@ const box = document.querySelectorAll(".box")
 
 box.forEach((box) => {
     box.addEventListener('click', () => {
-    
+    if(box.innerHTML == ''){
     if(player){
         box.innerText = 'X'
         player = false;
+        
         }
 
     else{
         box.innerHTML = 'O'
         player = true;
+        
     }
-    console.log('e')    
+    checkWin()
+}
+     
 })
 })
+
+const checkWin = () => {
+    for (let pattern of winningPatterns) {
+        let pos1 = box[pattern[0]].innerText;
+        let pos2 = box[pattern[1]].innerText;
+        let pos3 = box[pattern[2]].innerText;
+
+        if (pos1 !== '' && pos2 !== '' && pos3 !== '') {
+            if (pos1 === pos2 && pos2 === pos3) {
+                console.log(`winner ${pos1}` );
+                disableBox()
+                
+            }
+        }
+    }
+};
+
+const disableBox = () => {
+    box.forEach((box) => {
+        box.style.pointerEvents = 'none';
+    });
+};
